fe608756fcd8a3956a080ea92fb63f70ef66fb2c
Jonathan Fink <collegepulsemag@gmail.com>
2019-04-24 01:31:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  if (string === whisper){
+  if (string === lowercase){
     return "I can't hear you!"
   }
-  else if (string === shout) {
+  else if (string === uppercase) {
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma."){
